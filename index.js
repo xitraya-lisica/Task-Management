d9fdb21fe9c8a01f82cd7f454bf21b78d0511268
xitraya-lisica <xitraya_lisica@ukr.net>
2025-03-06 20:53:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ let tasks = [
 const requestHandler = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const method = req.method;
+
+    
     
     // Set headers for the response
     res.setHeader('Content-Type', 'application/json');
